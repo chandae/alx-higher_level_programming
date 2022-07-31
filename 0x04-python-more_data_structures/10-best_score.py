ad84712f21caab5b841e02905da1da36df8763b2
Chandae <emmanuelvchanda1@gmail.com>
2022-07-31 16:00:59 +0200

    Update: new file 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,5 +5,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    if not a_dictionary:
+        return None
     new_dict = sorted(a_dictionary.items(), key=lambda pair: pair[1])
     return new_dict[-1][0]
