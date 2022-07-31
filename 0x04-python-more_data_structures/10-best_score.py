b8a8083e643ccf19c2a15b439b9c8c92e20e23f5
Chandae <emmanuelvchanda1@gmail.com>
2022-07-31 15:58:10 +0200

    Update: new file 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Returns a key with the largest integer value """
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    new_dict = sorted(a_dictionary.items(), key=lambda pair: pair[1])
+    return new_dict[-1][0]
