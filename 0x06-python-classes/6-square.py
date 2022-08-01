3b16c9852e1162800a77d3d67c5e8829f6a868a8
Chandae <emmanuelvchanda1@gmail.com>
2022-08-01 22:46:48 +0200

    Update: new file added to repo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,20 +57,11 @@ class Square():
     def position(self, value):
         """ Setter method for private attribute position """
 
-        # Set private instance attribute position
         if (isinstance(value, tuple)) and len(value) == 2:
-            if isinstance(value[0], int) and isinstance(value[1], int):
-                if value[0] >= 0 and value[1] >= 0:
-                    self.__position = value
-                else:
-                    raise TypeError("position must be a \
-                        tuple of 2 positive integers")
-            else:
-                raise TypeError("position must be a \
-                    tuple of 2 positive integers")
+            if (isinstance(value[0], int) and isinstance(value[1], int)):
+                self.__position = value
         else:
-            raise TypeError("position must be a \
-                tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         """ Returns current square area """
