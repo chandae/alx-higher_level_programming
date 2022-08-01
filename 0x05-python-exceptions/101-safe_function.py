96812af34631e4f92f10da43036190435b17920d
Chandae <emmanuelvchanda1@gmail.com>
2022-08-01 14:26:29 +0200

    Update: new file added to repo

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Higher Order Function: safely prints another function """
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as err:
+        sys.stderr.write("Exception: " + str(err) + '\n')
+        return None
