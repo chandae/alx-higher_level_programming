fb1aacdc2d9747bfa25f665bad034cdb295f1e4c
Chandae <emmanuelvchanda1@gmail.com>
2022-08-01 21:17:59 +0200

    Update: new file added to repo

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" My square module """
+
+
+class Square():
+    """ Square class """
+
+    """ Constructor """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """ Returns current square area """
+    def area(self):
+        return self.__size ** 2
