529be6ca354db82073dac5511f1d8fea10bdbc3b
Chandae <emmanuelvchanda1@gmail.com>
2022-08-01 14:17:45 +0200

    Update: new file added to repo

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Function prints integer """
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as err:
+        sys.stderr.write('Exception: ' + str(err) + '\n')
+        return False
