6384ae55dfec60cd5dd154fd9853cfffdef2a4ba
Chandae <emmanuelvchanda1@gmail.com>
2022-08-01 22:24:50 +0200

    Update: new file added to repo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,10 +11,17 @@ class Square():
         # Set private instance attribute size
         if not (isinstance(size, int)):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+        else:
+            self.__size = size
+
+        # Set private instance attribute position
+        if (isinstance(position, tuple)) and len(position) == 2:
+            if (isinstance(position[0], int) and isinstance(position[1], int)):
+                self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
