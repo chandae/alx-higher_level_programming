927146f215d44c8ea1a220390d6340b003cd9c99
Chandae <emmanuelvchanda1@gmail.com>
2022-08-03 20:56:03 +0200

    New file added to repo

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    3-say_my_name: say_my_name(first_name, last_name="")
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        Prints 'My name is <first_name> <last_name>
+
+        Args:
+            first_name: first argument
+            last_name: second argument (positional)
+        Returns:
+            None
+    """
+
+    if not (isinstance(first_name, str)) or not first_name:
+        raise TypeError("first_name must be a string")
+    if not (isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
