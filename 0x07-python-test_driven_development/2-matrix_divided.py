2443d689a21cb10f43e0c20a66833b70adcb1f41
Chandae <emmanuelvchanda1@gmail.com>
2022-08-03 20:25:23 +0200

    Update: making copy of matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,21 +15,21 @@ def matrix_divided(matrix, div):
             new matrix with each element divided by div
     """
 
-    my_matrix = matrix.copy()
+    new_matrix = []
     error_1 = "matrix must be a matrix (list of lists) of integers/floats"
     error_2 = "Each row of the matrix must have the same size"
 
     # Matrix must be a list of lists of integers or floats
-    if not (isinstance(my_matrix, list)) or not matrix:
+    if not (isinstance(matrix, list)) or not matrix:
         raise TypeError(error_1)
-    for row in my_matrix:
+    for row in matrix:
         for element in row:
             if type(element) not in [int, float]:
                 raise TypeError(error_1)
 
     # Each row of the matrix must be the same size
-    row_length = len(my_matrix[0])
-    for row in my_matrix:
+    row_length = len(matrix[0])
+    for row in matrix:
         if len(row) != row_length:
             raise TypeError(error_2)
 
@@ -40,8 +40,8 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     # Divide the elements by div
-    for i, row in enumerate(my_matrix):
-        for j, num in enumerate(row):
-            my_matrix[i][j] = float("{:.2f}".format(num / div))
+    for row in matrix:
+        temp = [float("{:.2f}".format(num / div)) for num in row]
+        new_matrix.append(temp)
 
-    return my_matrix
+    return new_matrix
