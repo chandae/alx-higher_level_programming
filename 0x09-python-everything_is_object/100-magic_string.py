fc696be24d869cfcc436248d128650d38e612589
chandae <emmanuelvchanda1@gmail.com>
2022-08-05 18:50:50 +0200

    Update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def magic_string():
-    itr = list(globals().items())[-1][1]
-    return 'BestSchool, ' * itr + 'BestSchool'
+    return 'BestSchool, ' * globals()['i'] + 'BestSchool'
