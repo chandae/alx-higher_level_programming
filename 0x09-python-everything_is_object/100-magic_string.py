528aaa36444549dc2b703decae3f1e82c4a534c5
chandae <emmanuelvchanda1@gmail.com>
2022-08-05 18:12:19 +0200

    Update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    x, y = i != 0, "BestSchool"
-    return ''.join(["BestSchool, " for _ in range(i + 1)]).strip() if x else y
+    global i
+    return ''.join(["BestSchool, " for _ in range(i + 1)]).strip()
