186d1491f35f32f8c6a0cbe0048ce730deceb523
chandae <emmanuelvchanda1@gmail.com>
2022-08-06 14:19:30 +0200

    Update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Base Geometry Module """
+
+
+class BaseGeometry:
+    """ Base Geometry """
+
+    def area(self):
+        """ Undefined """
+        raise Exception("area() is not defined")
