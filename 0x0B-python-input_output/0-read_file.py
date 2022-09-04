6dd06acc77e0441f7bfe48f789c0aec9ec321e9a
Chandae <emmanuelvchanda1@gmail.com>
2022-09-05 00:04:14 +0200

    adhere to pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
     0-read_file: read_file()
 """
 
+
 def read_file(filename=""):
     """ Reads text file (utf-8) and outputs contents to stdout """
     with open(filename, 'r', encoding='utf-8') as f:
