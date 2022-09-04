a619e1501e8e1c59d5a0e20ac5504708ef5e0141
Chandae <emmanuelvchanda1@gmail.com>
2022-09-05 01:01:48 +0200

    Update: add new file to repo

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -13,4 +13,4 @@ def load_from_json_file(filename):
             filename -  name of file to get object from
     """
     with open(filename, 'r', encoding='utf-8') as json_file:
-        json.load(json_file)
+        return json.load(json_file)
