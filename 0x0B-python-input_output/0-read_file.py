c2047cf980f62fbf41cdde0f9e70b961d62a4aba
Chandae <emmanuelvchanda1@gmail.com>
2022-09-04 23:52:36 +0200

    Quick code fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    """ Reads text file (utg-8) and outputs contents to stdout """
+    """ Reads text file (utf-8) and outputs contents to stdout """
     with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+        print(f.read())
