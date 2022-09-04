e4344ca429ed3d47fa77d9efc37863e157d333d5
Chandae <emmanuelvchanda1@gmail.com>
2022-09-04 23:47:03 +0200

    code fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 
 def read_file(filename=""):
     """ Reads text file (utg-8) and outputs contents to stdout """
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             print(line, end='')
