6e3b9bce617bff8ec06ff0df61973fc0eab26359
Chandae <emmanuelvchanda1@gmail.com>
2022-09-05 23:28:28 +0200

    Commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,10 +4,8 @@
 
 if (process.argv.length === 2) {
   console.log('No argument');
-}
-if (process.argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
-}
-if (process.argv.length > 3) {
+} else if (process.argv.length > 3) {
   console.log('Arguments found');
 }
