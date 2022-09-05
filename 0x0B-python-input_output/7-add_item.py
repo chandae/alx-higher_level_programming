dbcbd5af99d2cdef4caffe7c21ebb5cbb7b0c3ca
Chandae <emmanuelvchanda1@gmail.com>
2022-09-05 02:01:55 +0200

    Update: new file to repo

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,42 +2,19 @@
 """
     Loads script arguments to list and then saves list to json file
 """
-import json
 import sys
 
 
-def load_from_json_file(filename):
-    """
-        Creates python oject from json file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-        Args:
-            filename -  name of file to get object from
-    """
-    with open(filename, 'r', encoding='utf-8') as json_file:
-        return json.load(json_file)
+filename = "add_item.json"
 
+try:
+    arg_list = load_from_json_file(filename)
+except Exception as err:
+    arg_list = []
 
-def save_to_json_file(my_obj, filename):
-    """
-        Writes and saves json object to a text file
-
-        Args:
-            my_obj -  object to write to text file
-            filename -  name of file to write to
-    """
-    with open(filename, 'w', encoding='utf-8') as json_file:
-        json.dump(my_obj, json_file)
-
-
-if __name__ == "__main__":
-
-    filename = "add_item.json"
-
-    try:
-        arg_list = load_from_json_file(filename)
-    except Exception as err:
-        arg_list = []
-
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
+for arg in sys.argv[1:]:
+    arg_list.append(arg)
+save_to_json_file(arg_list, filename)
