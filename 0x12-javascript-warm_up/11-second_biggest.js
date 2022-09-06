f3928858c2bd9c606222fddb3da98d0f0627ee22
Chandae <emmanuelvchanda1@gmail.com>
2022-09-06 16:30:51 +0200

    Update: new file 11-second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,10 @@
 if (process.argv.length < 4) {
   console.log('0');
 } else {
-  const args = process.argv.slice(2, -1);
-  args.sort();
+  const args = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    args[i - 2] = process.argv[i];
+  }
+  args.sort(function (a, b) { return b - a; });
   console.log(args[1]);
 }
