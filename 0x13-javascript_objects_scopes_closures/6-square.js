cb27cef397e6ab000b4711a590e753e29921b11b
Chandae <emmanuelvchanda1@gmail.com>
2022-09-07 07:33:52 +0200

    Update: new file 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const Square1 = require('./5-square');
+
+module.expoers = class Square extends Square1 {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    this.print(c);
+  }
+};
