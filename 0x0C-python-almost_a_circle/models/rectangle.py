00bc020b7ca271968c09d0690d0eb991bf6f27cf
Chandae <emmanuelvchanda1@gmail.com>
2022-09-08 01:03:31 +0200

    Update: quick fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,7 +21,7 @@ class Rectangle(Base):
 
     def __str__(self) -> str:
         """ Print rectangle object """
-        return "{} ({}) {}/{} - {}/{}".format(
+        return "[{}] ({}) {}/{} - {}/{}".format(
             type(self).__name__, self.id, self.x,
             self.y, self.width, self.height
             )
