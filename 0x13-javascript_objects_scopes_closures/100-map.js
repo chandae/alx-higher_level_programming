17898f2ffedc32198a0f97a931dfbbd78ea053e6
Chandae <emmanuelvchanda1@gmail.com>
2022-09-07 21:50:11 +0200

    Update: new file

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
-const newList = list.map((element, index) => element * index);
+const newList = list.map(function (value, index) { return value * index; });
 console.log(list);
 console.log(newList);
