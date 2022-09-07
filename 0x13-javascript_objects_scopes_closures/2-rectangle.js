77623c3bdb1a203bb414289fa01c2b5a22ef72d6
Chandae <emmanuelvchanda1@gmail.com>
2022-09-07 06:32:24 +0200

    Update: quick code fiz

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (!isNaN(Number(w)) || !isNaN(Number(h))) {
+    if (!isNaN(Number(w)) && !isNaN(Number(h))) {
       if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
