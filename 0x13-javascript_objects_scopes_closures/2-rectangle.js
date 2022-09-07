29124e7ba2d94cea8cd3bc9fa9b8ecadd9b9f55e
Chandae <emmanuelvchanda1@gmail.com>
2022-09-07 06:30:59 +0200

    Update: fix semistandard issue

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor(w, h) {
-        if (!isNaN(Number(w)) || !isNaN(Number(h))) {
-            if (w > 0 && h > 0) {
-                this.width = w;
-                this.height = h;
-            }
-        }
+  constructor (w, h) {
+    if (!isNaN(Number(w)) || !isNaN(Number(h))) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
     }
-}
+  }
+};
