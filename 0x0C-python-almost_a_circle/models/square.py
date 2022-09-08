8fd679ec5f6487b7d17a10e22b5033e920b84199
Chandae <emmanuelvchanda1@gmail.com>
2022-09-08 18:16:39 +0200

    Update: new file square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Square class: child class of Rectangle """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+    def __init__(self, size, x=0, y=0, id=None) -> None:
+        """ Constructor """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self) -> str:
+        """ Print rectangle object """
+        return "[{}] ({}) {}/{} - {}".format(
+            type(self).__name__, self.id,
+            self.x, self.y, self.width
+            )
