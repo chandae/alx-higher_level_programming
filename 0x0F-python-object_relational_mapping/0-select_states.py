70a3e58d430473c6f7479debc19b7603aada6fe8
Chandae <emmanuelvchanda1@gmail.com>
2022-09-13 14:18:25 +0200

    Update: new commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""
+    List all states from a given db sorted in ascending order by id
+    Username, password, and database names are given as user args
+"""
 import sys
+import MySQLdb
 
 
-if __name__ == '__main__':
-    # fetch cmdline arguments database username, password, db_name
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
                          host='localhost',
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    queries = cur.fetchall()
-    for query in queries:
-        print(query)
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    allStates = cur.fetchall()
+
+    for state in allStates:
+        print(state)
+
     cur.close()
     db.close()
