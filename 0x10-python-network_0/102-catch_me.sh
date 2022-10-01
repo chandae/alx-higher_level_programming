b817e62ea8abdb79d65f053f424a4ff2bcf4493e
Chandae <emmanuelvchanda1@gmail.com>
2022-10-01 07:58:50 +0200

    Initial file commit

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script makes request to 0.0.0.0:5000/catch_me
-curl -s -w 'You got me!' 8890b4723545.0edfdbca.alx-cod.online:5000/catch_me -o saved
+curl -s -w 'You find me!' 0.0.0.0:5000/catch_me -o saved
