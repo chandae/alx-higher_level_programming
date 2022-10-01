2216b1fe99f63a36387c8c6fbf09b7e8ec05a809
Chandae <emmanuelvchanda1@gmail.com>
2022-10-01 09:06:15 +0200

    Initial file commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Finds peak element in a given array """
+
+
+def find_peak(array):
+    """ find the peak element in my_list """
+    n = len(array)
+    if n == 0:
+        return
+    mid = n // 2
+
+    check_1 = (mid == 0 or array[mid - 1] <= array[mid])
+    check_2 = (mid == n - 1 or array[mid + 1] < array[mid])
+    if check_1 and check_2:
+        return array[mid]
+    elif (mid > 0 and array[mid - 1] > array[mid]):
+        return find_peak(array[:mid])
+    else:
+        return find_peak(array[mid:])
