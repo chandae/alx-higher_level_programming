480fa82051fd2118a015cbbaa6fb4a4b719a97b9
Chandae <emmanuelvchanda1@gmail.com>
2022-10-13 16:42:53 +0200

    Update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,10 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    def to_json_string(list_dict):
+        """ returns json string representation """
+        if list_dict is None or list_dict == {}:
+            return "[]"
+        else:
+            return str(list_dict)
