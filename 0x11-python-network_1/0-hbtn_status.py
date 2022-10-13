dd2a8722fc747e77900ac5ff67f7e1064f538e47
Chandae <emmanuelvchanda1@gmail.com>
2022-10-13 15:55:20 +0200

    Update: new project files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+
 """ script fetches https://intranet.htbn.io/status """
 import urllib.request
 
 
 if __name__ == '__main__':
-    url = 'https://intranet.htbn.io/status'
+    url = 'https://d6b4436ad043.413a0140.alx-cod.online:5000'
     with urllib.request.urlopen(url) as response:
         html = response.read()
 
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        print(response.getheader('X-Request-Id'))
+        print(response.info().get('X-Request-Id'))
