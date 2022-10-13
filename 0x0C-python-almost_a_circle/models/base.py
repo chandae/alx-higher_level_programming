c9ae096ca04f751f2d41e6186104b65e1e449b79
Chandae <emmanuelvchanda1@gmail.com>
2022-10-13 16:45:42 +0200

    make to_json_string static

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,7 +13,8 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
+    @staticmethod
     def to_json_string(list_dict):
         """ returns json string representation """
         if list_dict is None or list_dict == {}:
